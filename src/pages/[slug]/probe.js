489f69be80c966bceb9098b70bd402f3fe56a5ec
Shaun Hall <shaun.hall@thehutgroup.com>
2025-04-07 14:29:58 +0100

    add telemetry to the horizon call only

--- src/pages/[slug]/probe.js
+++ src/pages/[slug]/probe.js
@@ -1,20 +1,20 @@
 export const GET = async ({ params, request }) => {
     const response = await fetchWithTimeout("https://horizon-api.www.lookfantastic.com/schema-docs")
-
-    return new Response(response.body, {headers:{'Cache-Control': 'max-age=0, no-cache, no-store'}})
+    return new Response(response.body, {headers: {...Object.fromEntries(response.headers), ...{'Cache-Control': 'max-age=0, no-cache, no-store'}}})
   }
 
 async function fetchWithTimeout(resource, timeout = 3000) {  
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), timeout);
-
+  let requestStartTime = new Date().getTime(),
+      requestEndTime
   const response = await fetch(resource, {
     headers: {
       'X-Horizon-Client': 'altitude-shield-prober'
     },
     signal: controller.signal  
   });
+  requestEndTime = new Date().getTime()
   clearTimeout(id);
-
-  return response;
+  return new Response(response.body, {headers:{'X-Horizon-Duration': `${requestEndTime - requestStartTime}`}})
 }
